3d217524e7d5c95fd126d0a6809f90ede1e2c888
Martin Outzen Berild <martinob@null05.math.ntnu.no>
2020-02-28 17:07:08 +0100

    add folder for survival model example

---
+++ survival/weibull.R
@@ -0,0 +1,33 @@
+# there are two different parametrizations in inla for the weibull
+library(INLA)
+variant  = 0
+
+n = 1000
+
+u = rep(rnorm(100,mean = 0, sd = 3), each = 10)
+
+alpha = 1.1
+beta = 2.2
+x = c(scale(runif(n)))
+eta = 1+beta*x + u
+lambda = exp(eta)
+
+y = rweibull(n,
+               shape = alpha,
+               scale = lambda^(-1/alpha))
+# this is the censoring, we set it to 1 so all 
+#failure times are observed             
+event = rep(1,n) 
+
+data = list(y=y, event=event, x=x, idx = rep(1:100,each = 10))
+
+## the function inla.surv() creates a new dataset that takes into account the 
+## censoring, the likelihood contribution of the data point changes 
+## according to if we have censoring or not
+
+formula = inla.surv(y,event)~ x + f(idx, model = "iid")
+r=inla(formula,
+       family ="weibullsurv",
+       data=data,
+       control.family = list(list(variant = variant)))
+plot(r)
