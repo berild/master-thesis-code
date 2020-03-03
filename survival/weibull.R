731590344744d7df4fce007f7b8dae2537b9315c
Martin Outzen Berild <martinob@null05.math.ntnu.no>
2020-03-03 17:18:40 +0100

    fix fit.inla function with offset in frailty model

--- survival/weibull.R
+++ survival/weibull.R
@@ -2,9 +2,9 @@
 library(INLA)
 variant  = 0
 
-n = 1000
+n = 100
 
-u = rep(rnorm(100,mean = 0, sd = 3), each = 10)
+u = rep(rnorm(10,mean = 0, sd = 3), each = 10)
 
 alpha = 1.1
 beta = 2.2
@@ -19,7 +19,7 @@ y = rweibull(n,
 #failure times are observed             
 event = rep(1,n) 
 
-data = list(y=y, event=event, x=x, idx = rep(1:100,each = 10))
+data = list(y=y, event=event, x=x, idx = rep(1:10,each = 10))
 
 ## the function inla.surv() creates a new dataset that takes into account the 
 ## censoring, the likelihood contribution of the data point changes 
@@ -32,8 +32,20 @@ r=inla(formula,
        control.family = list(list(variant = variant)))
 plot(r)
 
+fit.inla <- function(data,eta){
+  oset = eta[data$idx]
+  formula = inla.surv(y,event) ~ 1 + x + offset(log(oset))
+  res=inla(formula,
+           family ="weibullsurv",
+           data=data,
+           control.family = list(list(variant = variant)))
+  return(list(mlik = res$mlik[[1]],
+              dists = list(intercept = res$marginals.fixed[[1]],
+                           beta = res$marginals.fixed[[2]])))
+}
+
 plot_gamma <- function(alpha, n){
   return(ggplot() + 
            geom_line(data = data.frame(x = seq(0,3*alpha*alpha,length.out = n),y = dgamma(seq(0,3*alpha*alpha,length.out = n),scale =alpha, shape = alpha)), aes(x = x, y = y)))
 }
-plot_gamma(alpha = 4, n =20)
+plot_gamma(alpha = 3, n =20)
