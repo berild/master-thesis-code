e03009980c881574fe02c46e20c83a90a97daba8
berild <berild@github.com>
2020-06-01 15:30:00 +0200

    fix dimension issues of is with inla sem problem

--- sem/is_w_inla.R
+++ sem/is_w_inla.R
@@ -24,23 +24,23 @@ par.is <- function(x, data, theta, t, prior, d.prop, r.prop, fit.inla){
     },error=function(e){
     },finally={})
   }
-  weight = mod$mlik + prior(eta) - d.prop(y = eta, x = theta$a.mu[t], sigma = theta$a.cov[t])
+  weight = mod$mlik + prior(eta) - d.prop(y = eta, x = theta$a.mu[t,], sigma = theta$a.cov[,,t])
   return(list(mlik = mod$mlik, dists = mod$dists, eta = eta, weight = weight, times = Sys.time()))
 }
 
 
 is.w.inla <- function(data, init, prior, d.prop, r.prop, fit.inla, N_0 = 200, N = 400){
-  eta = rep(NA,  N_0)
-  weight = numeric(N_0)
   if (detectCores()>10){
     ncores = 10
   }else{
     ncores = detectCores()
   }
-  theta = list(a.mu = rep(NA, 2),
-               a.cov = rep(NA, 2))
-  theta$a.mu[1] = init$mu
-  theta$a.cov[1] = init$cov
+  eta = matrix(NA, ncol = length(init$mu), nrow = N_0)
+  weight = numeric(N_0)
+  theta = list(a.mu = matrix(NA, ncol = length(init$mu), nrow = 2),
+               a.cov = array(NA, dim = c(length(init$mu), length(init$mu), 2)))
+  theta$a.mu[1,] = init$mu
+  theta$a.cov[,,1] = init$cov
   starttime = Sys.time()
   times = numeric(N)
   pb <- txtProgressBar(min = 0, max = N+N_0, style = 3)
@@ -49,12 +49,12 @@ is.w.inla <- function(data, init, prior, d.prop, r.prop, fit.inla, N_0 = 200, N 
   }, mc.set.seed = TRUE, mc.cores = ncores)
   for (i in seq(length(is.list))){
     setTxtProgressBar(pb, i)
-    eta[i] = is.list[[i]]$eta
+    eta[i,]= is.list[[i]]$eta
     weight[i] = is.list[[i]]$weight
   }
   theta = calc.theta(theta,weight,eta,length(eta),2)
   margs = NA
-  eta = rep(NA, N)
+  eta = matrix(NA, ncol = length(init$mu), nrow = N_0)
   weight = numeric(N)
   mlik = numeric(N)
   is.list = mclapply(seq(N), function(x){
@@ -62,7 +62,7 @@ is.w.inla <- function(data, init, prior, d.prop, r.prop, fit.inla, N_0 = 200, N 
   }, mc.set.seed = TRUE, mc.cores = ncores)
   for (i in seq(length(is.list))){
     setTxtProgressBar(pb, i+N_0)
-    eta[i] = is.list[[i]]$eta
+    eta[i,] = is.list[[i]]$eta
     margs = store.post(is.list[[i]]$dists,margs,i,N)
     weight[i] = is.list[[i]]$weight
     mlik[i] = is.list[[i]]$mlik
