df98cda4adafb337f648ade05feaeea4a55c8ad6
berild <berild@github.com>
2020-06-01 13:32:23 +0200

    add is with inla simulation

---
+++ sem/is_w_inla.R
@@ -0,0 +1,77 @@
+require(INLA)
+require(spdep)
+require(INLABMA)
+require(parallel)
+require(mvtnorm)
+
+dq.rho.lambda <- function(y, x, sigma = init$cov, log =TRUE) {
+  dmvt(y,sigma = sigma, df=3, delta = x, type = "shifted",log=log)
+  #dmvnorm(y, mean = x, sigma = sigma, log = log)
+}
+
+rq.rho.lambda <- function(x, sigma = init$cov) {
+  as.vector(rmvt(1,sigma = sigma, df=3, delta = x, type = "shifted"))
+  #as.vector(rmvnorm(1, mean = x, sigma = sigma))
+}
+
+par.is <- function(x, data, theta, t, prior, d.prop, r.prop, fit.inla){
+  INLA_crash = T
+  while(INLA_crash){
+    tryCatch({
+      eta = r.prop(theta$a.mu[t,], sigma = theta$a.cov[,,t])
+      mod = fit.inla(data = data ,eta = eta)
+      INLA_crash = F 
+    },error=function(e){
+    },finally={})
+  }
+  weight = mod$mlik + prior(eta) - d.prop(y = eta, x = theta$a.mu[t], sigma = theta$a.cov[t])
+  return(list(mlik = mod$mlik, dists = mod$dists, eta = eta, weight = weight, times = Sys.time()))
+}
+
+
+is.w.inla <- function(data, init, prior, d.prop, r.prop, fit.inla, N_0 = 200, N = 400){
+  eta = rep(NA,  N_0)
+  weight = numeric(N_0)
+  if (detectCores()>10){
+    ncores = 10
+  }else{
+    ncores = detectCores()
+  }
+  theta = list(a.mu = rep(NA, 2),
+               a.cov = rep(NA, 2))
+  theta$a.mu[1] = init$mu
+  theta$a.cov[1] = init$cov
+  starttime = Sys.time()
+  times = numeric(N)
+  pb <- txtProgressBar(min = 0, max = N+N_0, style = 3)
+  is.list = mclapply(seq(N_0), function(x){
+    par.is(x, data, theta, 1, prior, d.prop,r.prop, fit.inla)
+  }, mc.set.seed = TRUE, mc.cores = ncores)
+  for (i in seq(length(is.list))){
+    setTxtProgressBar(pb, i)
+    eta[i] = is.list[[i]]$eta
+    weight[i] = is.list[[i]]$weight
+  }
+  theta = calc.theta(theta,weight,eta,length(eta),2)
+  margs = NA
+  eta = rep(NA, N)
+  weight = numeric(N)
+  mlik = numeric(N)
+  is.list = mclapply(seq(N), function(x){
+    par.is(x, data, theta, 2, prior, d.prop,r.prop, fit.inla)
+  }, mc.set.seed = TRUE, mc.cores = ncores)
+  for (i in seq(length(is.list))){
+    setTxtProgressBar(pb, i+N_0)
+    eta[i] = is.list[[i]]$eta
+    margs = store.post(is.list[[i]]$dists,margs,i,N)
+    weight[i] = is.list[[i]]$weight
+    mlik[i] = is.list[[i]]$mlik
+    times[i] = as.numeric(difftime(is.list[[i]]$times,starttime,units = "secs"))
+  }
+  weight = exp(weight - max(weight))
+  return(list(eta = eta,
+              theta = theta,
+              margs = lapply(margs, function(x){fit.marginals(weight,x)}),
+              weight = weight,
+              times = times))
+}
--- sem/sem.R
+++ sem/sem.R
@@ -38,34 +38,42 @@ amis_w_inla_mod <- amis.w.inla(data = turnout, init = init, prior.rho.lambda,
                                dq.rho.lambda, rq.rho.lambda, fit.inla, 
                                N_t = seq(25,50,1), N_0 = 25)
 save(amis_w_inla_mod, file = "./sims/sem-amis-w-inla.Rdata")
-amis_w_inla_mod$weight = exp(amis_w_inla_mod$weight-max(amis_w_inla_mod$weight))
-eta_kern = kde2d.weighted(x = amis_w_inla_mod$eta[,1], y = amis_w_inla_mod$eta[,2], w = amis_w_inla_mod$weight/(sum(amis_w_inla_mod$weight)), n = 100, lims = c(-1,1,-1,1))
-eta_kern = data.frame(expand.grid(x=eta_kern$x, y=eta_kern$y), z=as.vector(eta_kern$z))
+eta_kern_amis = kde2d.weighted(x = amis_w_inla_mod$eta[,1], y = amis_w_inla_mod$eta[,2], w = amis_w_inla_mod$weight/(sum(amis_w_inla_mod$weight)), n = 100, lims = c(-1,1,-1,1))
+amis_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_amis$x, y=eta_kern_amis$y), z=as.vector(eta_kern_amis$z))
+save(amis_w_inla_mod, file = "./sims/sem-amis-w-inla.Rdata")
 
-p3 <- ggplot() + 
-  #geom_point(data = data.frame(x = 2, y = -2), aes(x = x, y = y), shape = 4,size = 3) + 
-  #geom_text(data = data.frame(x = 2, y = -2), aes(x = x, y = y), label="True Value", vjust=2) + 
-  #geom_line(data = data.frame(x=rep(1000,10),y = rep(1000,10),type = "INLA"), aes(x=x,y=y,linetype=type))+
-  geom_contour(data = eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA"),bins = 6) + 
-  #geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) + 
-  #geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA"),bins = 6) + 
-  labs(color = "",x=expression(rho),y=expression(lambda),linetype="") + 
-  #coord_cartesian(xlim = c(1.2,2.7),ylim=c(-2.7,-1.3))+
-  theme_bw() + 
-  theme(legend.position="bottom")
-p3
-#source("./sem/sem_is_w_inla.R")
-#is_w_inla_mod <- is.w.inla(data = df, init = init, prior.rho, 
-#                           dq.rho, rq.rho,fit.inla, N_0 = 800, N = 10000)
-#save(is_w_inla_mod, file = "./sem/sims/sem-is-w-inla.Rdata")
-#
-#source("./sem/sem_mcmc_w_inla.R")
-#mcmc_w_inla_mod <- mcmc.w.inla(data = df, init = init$mu,
-#                               prior.rho, dq.rho, rq.rho, fit.inla,
-#                               n.samples = 100500, n.burnin = 500, n.thin = 10)
-#save(mcmc_w_inla_mod, file = "./sem/sims/sem-mcmc-w-inla.Rdata")
+source("./sem/sem_is_w_inla.R")
+is_w_inla_mod <- is.w.inla(data = df, init = init, prior.rho,
+                          dq.rho, rq.rho,fit.inla, N_0 = 800, N = 10000)
+save(is_w_inla_mod, file = "./sims/sem-is-w-inla.Rdata")
+eta_kern_is = kde2d.weighted(x = is_w_inla_mod$eta[,1], y = is_w_inla_mod$eta[,2], w = is_w_inla_mod$weight/(sum(is_w_inla_mod$weight)), n = 100, lims = c(-1,1,-1,1))
+is_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_is$x, y=eta_kern_is$y), z=as.vector(eta_kern_is$z))
+save(is_w_inla_mod, file = "./sims/sem-is-w-inla.Rdata")
 
-
-spplot(turnout["TURNOUT01"],  col.regions= hcl.colors(16,palette =  hcl.pals("sequential")[21], alpha = 1, rev = TRUE),colorkey = list(space = "bottom"))
-
-spplot(turnout["GDPCAP"], col.regions= hcl.colors(16,palette =  hcl.pals("sequential")[21], alpha = 1, rev = TRUE),colorkey = list(space = "bottom"))
+# p3 <- ggplot() + 
+#   #geom_point(data = data.frame(x = 2, y = -2), aes(x = x, y = y), shape = 4,size = 3) + 
+#   #geom_text(data = data.frame(x = 2, y = -2), aes(x = x, y = y), label="True Value", vjust=2) + 
+#   #geom_line(data = data.frame(x=rep(1000,10),y = rep(1000,10),type = "INLA"), aes(x=x,y=y,linetype=type))+
+#   geom_contour(data = eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA"),bins = 6) + 
+#   #geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) + 
+#   #geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA"),bins = 6) + 
+#   labs(color = "",x=expression(rho),y=expression(lambda),linetype="") + 
+#   #coord_cartesian(xlim = c(1.2,2.7),ylim=c(-2.7,-1.3))+
+#   theme_bw() + 
+#   theme(legend.position="bottom")
+# p3
+# #source("./sem/sem_is_w_inla.R")
+# #is_w_inla_mod <- is.w.inla(data = df, init = init, prior.rho, 
+# #                           dq.rho, rq.rho,fit.inla, N_0 = 800, N = 10000)
+# #save(is_w_inla_mod, file = "./sem/sims/sem-is-w-inla.Rdata")
+# #
+# #source("./sem/sem_mcmc_w_inla.R")
+# #mcmc_w_inla_mod <- mcmc.w.inla(data = df, init = init$mu,
+# #                               prior.rho, dq.rho, rq.rho, fit.inla,
+# #                               n.samples = 100500, n.burnin = 500, n.thin = 10)
+# #save(mcmc_w_inla_mod, file = "./sem/sims/sem-mcmc-w-inla.Rdata")
+# 
+# 
+# spplot(turnout["TURNOUT01"],  col.regions= hcl.colors(16,palette =  hcl.pals("sequential")[21], alpha = 1, rev = TRUE),colorkey = list(space = "bottom"))
+# 
+# spplot(turnout["GDPCAP"], col.regions= hcl.colors(16,palette =  hcl.pals("sequential")[21], alpha = 1, rev = TRUE),colorkey = list(space = "bottom"))
