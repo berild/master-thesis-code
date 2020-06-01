a232f377fae0f03b34bda29564d2950a40c59275
berild <berild@github.com>
2020-06-01 21:31:14 +0200

    add simulation of inlamh

--- sem/sem.R
+++ sem/sem.R
@@ -33,15 +33,15 @@ init = list(mu = c(0,0),cov = diag(2))
 
 source("./sem/general_functions.R")
 
-source("./sem/amis_w_inla.R")
-amis_w_inla_mod <- amis.w.inla(data = turnout, init = init, prior.rho.lambda,
-                               dq.rho.lambda, rq.rho.lambda, fit.inla,
-                               N_t = seq(25,50,1)*10, N_0 = 250)
-save(amis_w_inla_mod, file = "./sims/sem-amis-w-inla.Rdata")
-eta_kern_amis = kde2d.weighted(x = amis_w_inla_mod$eta[,1], y = amis_w_inla_mod$eta[,2], w = amis_w_inla_mod$weight/(sum(amis_w_inla_mod$weight)), n = 100, lims = c(-1,1,-1,1))
-amis_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_amis$x, y=eta_kern_amis$y), z=as.vector(eta_kern_amis$z))
-save(amis_w_inla_mod, file = "./sims/sem-amis-w-inla.Rdata")
-
+# source("./sem/amis_w_inla.R")
+# amis_w_inla_mod <- amis.w.inla(data = turnout, init = init, prior.rho.lambda,
+#                                dq.rho.lambda, rq.rho.lambda, fit.inla,
+#                                N_t = seq(25,50,1)*10, N_0 = 250)
+# save(amis_w_inla_mod, file = "./sims/sem-amis-w-inla.Rdata")
+# eta_kern_amis = kde2d.weighted(x = amis_w_inla_mod$eta[,1], y = amis_w_inla_mod$eta[,2], w = amis_w_inla_mod$weight/(sum(amis_w_inla_mod$weight)), n = 100, lims = c(-1,1,-1,1))
+# amis_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_amis$x, y=eta_kern_amis$y), z=as.vector(eta_kern_amis$z))
+# save(amis_w_inla_mod, file = "./sims/sem-amis-w-inla.Rdata")
+# 
 # source("./sem/is_w_inla.R")
 # is_w_inla_mod <- is.w.inla(data = turnout, init = init, prior.rho.lambda,
 #                            dq.rho.lambda, rq.rho.lambda,fit.inla, N_0 = 800, N = 10000)
@@ -49,59 +49,49 @@ save(amis_w_inla_mod, file = "./sims/sem-amis-w-inla.Rdata")
 # eta_kern_is = kde2d.weighted(x = is_w_inla_mod$eta[,1], y = is_w_inla_mod$eta[,2], w = is_w_inla_mod$weight/(sum(is_w_inla_mod$weight)), n = 100, lims = c(-1,1,-1,1))
 # is_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_is$x, y=eta_kern_is$y), z=as.vector(eta_kern_is$z))
 # save(is_w_inla_mod, file = "./sims/sem-is-w-inla.Rdata")
-# 
+
+init$cov = 0.1*init$cov
+source("./sem/sem_mcmc_w_inla.R")
+mcmc_w_inla_mod <- mcmc.w.inla(data = turnout, init = init,
+                               prior.rho.lambda, dq.rho.lambda, rq.rho.lambda, fit.inla,
+                               n.samples = 100500, n.burnin = 500, n.thin = 10)
+save(mcmc_w_inla_mod, file = "./sims/sem-mcmc-w-inla.Rdata")
+eta_kern_mcmc = kde2d.weighted(x = mcmc_w_inla_mod$eta[,1], y = mcmc_w_inla_mod$eta[,2], w = mcmc_w_inla_mod$weight/(sum(mcmc_w_inla_mod$weight)), n = 100, lims = c(-1,1,-1,1))
+mcmc_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_mcmc$x, y=eta_kern_mcmc$y), z=as.vector(eta_kern_mcmc$z))
+save(mcmc_w_inla_mod, file = "./sims/sem-mcmc-w-inla.Rdata")
+
+
 # library(ggplot2)
 # 
-# 
-# p1 <- ggplot() + 
-#   geom_line(data = amis_w_inla_mod$margs$intercept, aes(x=x,y=y,color="AMIS with INLA")) + 
-#   geom_line(data = is_w_inla_mod$margs$intercept, aes(x=x,y=y,color="IS with INLA")) + 
+# p1 <- ggplot() +
+#   geom_line(data = amis_w_inla_mod$margs$intercept, aes(x=x,y=y,color="AMIS with INLA")) +
+#   geom_line(data = is_w_inla_mod$margs$intercept, aes(x=x,y=y,color="IS with INLA")) +
 #   theme_bw() +
 #   theme(legend.position="bottom")
 # p1
 # 
 # 
-# p2 <- ggplot() + 
-#   geom_line(data = amis_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="AMIS with INLA")) + 
-#   geom_line(data = is_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="IS with INLA")) + 
+# p2 <- ggplot() +
+#   geom_line(data = amis_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="AMIS with INLA")) +
+#   geom_line(data = is_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="IS with INLA")) +
 #   theme_bw() +
 #   theme(legend.position="bottom")
 # p2
 # 
-# p3 <- ggplot() + 
-#   geom_line(data = amis_w_inla_mod$margs$tau, aes(x=x,y=y,color="AMIS with INLA")) + 
-#   geom_line(data = is_w_inla_mod$margs$tau, aes(x=x,y=y,color="IS with INLA")) + 
+# p3 <- ggplot() +
+#   geom_line(data = amis_w_inla_mod$margs$tau, aes(x=x,y=y,color="AMIS with INLA")) +
+#   geom_line(data = is_w_inla_mod$margs$tau, aes(x=x,y=y,color="IS with INLA")) +
 #   theme_bw() +
 #   theme(legend.position="bottom")
 # p3
 # 
 # p4 <- ggplot() +
-#   #geom_point(data = data.frame(x = 2, y = -2), aes(x = x, y = y), shape = 4,size = 3) +
-#   #geom_text(data = data.frame(x = 2, y = -2), aes(x = x, y = y), label="True Value", vjust=2) +
-#   #geom_line(data = data.frame(x=rep(1000,10),y = rep(1000,10),type = "INLA"), aes(x=x,y=y,linetype=type))+
 #   geom_contour(data = amis_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA"),bins = 6) +
-#   #geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) +
-#   #geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) +
-#   #geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA"),bins = 6) +
+#   geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) +
 #   labs(color = "",x=expression(rho),y=expression(lambda),linetype="") +
-#   #coord_cartesian(xlim = c(1.2,2.7),ylim=c(-2.7,-1.3))+
 #   theme_bw() +
 #   theme(legend.position="bottom")
 # p4
-# 
-
-
-# #source("./sem/sem_is_w_inla.R")
-# #is_w_inla_mod <- is.w.inla(data = df, init = init, prior.rho, 
-# #                           dq.rho, rq.rho,fit.inla, N_0 = 800, N = 10000)
-# #save(is_w_inla_mod, file = "./sem/sims/sem-is-w-inla.Rdata")
-# #
-# #source("./sem/sem_mcmc_w_inla.R")
-# #mcmc_w_inla_mod <- mcmc.w.inla(data = df, init = init$mu,
-# #                               prior.rho, dq.rho, rq.rho, fit.inla,
-# #                               n.samples = 100500, n.burnin = 500, n.thin = 10)
-# #save(mcmc_w_inla_mod, file = "./sem/sims/sem-mcmc-w-inla.Rdata")
-# 
 # 
 # spplot(turnout["TURNOUT01"],  col.regions= hcl.colors(16,palette =  hcl.pals("sequential")[21], alpha = 1, rev = TRUE),colorkey = list(space = "bottom"))
 # 
