e3ec50cbf486acc90f9dbf75785469bd26fc4484
berild <berild@github.com>
2020-06-03 15:00:29 +0200

    add ImmunogG to plotting functions

--- PQR/plot_pqr.R
+++ PQR/plot_pqr.R
@@ -66,6 +66,47 @@ ggplot() +
   theme_bw()
 
 
+# ImmunogG
+
+ggplot() + 
+  geom_line(data = as.data.frame(amis_w_inla_mod$margs$a), aes(x=x,y=y,color = "AMIS with INLA")) + 
+  #geom_line(data = as.data.frame(is_w_inla_mod$margs$a), aes(x=x,y=y,color = "IS with INLA")) + 
+  labs(color = "Method",title="a") + 
+  theme_bw()
+
+ggplot() + 
+  geom_line(data = as.data.frame(amis_w_inla_mod$margs$b), aes(x=x,y=y,color = "AMIS with INLA")) + 
+  #geom_line(data = as.data.frame(is_w_inla_mod$margs$b), aes(x=x,y=y,color = "IS with INLA")) + 
+  labs(color = "Method",title="b") + 
+  theme_bw()
+
+ggplot() + 
+  geom_line(data = amis_w_inla_mod$eta_kern[[1]],aes(x=x,y=y,color="AMIS with INLA")) + 
+  #geom_line(data = is_w_inla_mod$eta_kern[[1]],aes(x=x,y=y,color="IS with INLA")) + 
+  geom_vline(xintercept = amis_w_inla_mod$mod$params$c) + 
+  labs(color = "Method",title = "c") + 
+  theme_bw()
+
+
+ggplot() + 
+  geom_line(data = amis_w_inla_mod$eta_kern[[2]],aes(x=x,y=y,color="AMIS with INLA")) + 
+  #geom_line(data = is_w_inla_mod$eta_kern[[2]],aes(x=x,y=y,color="IS with INLA")) + 
+  geom_vline(xintercept = amis_w_inla_mod$mod$params$d) + 
+  labs(color = "Method",title = "d") + 
+  theme_bw()
+
+ggplot() + 
+  geom_line(data= amis_w_inla_mod$pqr,aes(x=x,y=y,color=quants,linetype="AMIS with INLA")) + 
+  #geom_line(data= is_w_inla_mod$pqr,aes(x=x,y=y,color=quants,linetype="IS with INLA")) + 
+  #geom_line(data = amis_w_inla_mod$pqr_truth,aes(x=x,y=y,color = quants,linetype="Truth")) + 
+  geom_point(data = as.data.frame(amis_w_inla_mod$mod),aes(x=x,y=y)) + 
+  labs(color="Quantiles",title="Gaussian M1",linetype="Method") + 
+  theme_bw()
+
+
+
+
+
 #DXX[DXX$mod=="D54",][-1],type ="gamma")
 tmp = DXX[DXX$mod=="D54",][-1]
 ggplot() +
