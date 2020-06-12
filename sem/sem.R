6d86e9962eff0a9f67afa309a2058f30ad98db03
Martin Outzen Berild <martinob@null08.math.ntnu.no>
2020-06-12 19:49:43 +0200

    add model defining functions and move plots to plot_sem.R script

--- sem/sem.R
+++ sem/sem.R
@@ -1,15 +1,13 @@
 library(spdep)
 library(spData)
+library(spatialreg)
 library(parallel)
 library(mvtnorm)
 library(MASS)
 library(coda)
-
-
 library(rgdal)
 library(sp)
 library(RColorBrewer)
-library(spdep)
 
 # LOad data
 load("./data/load_data.RData")
@@ -27,12 +25,50 @@ zero.variance <- list(prec = list(initial = 15, fixed = TRUE))
 n.xy <-  c(40, 20) #rho, lambda)
 
 turnout$idx <- 1:nrow(turnout)
-form <- TURNOUT01 ~ 1 + log(GDPCAP)
 
-init = list(mu = c(0,0),cov = diag(2))
+prior.rho.lambda <- function(x, log = TRUE) {
+  sum(dunif(x, -1, 1, log = log))
+}
 
 source("./sem/general_functions.R")
 
+fit.inla <- function(data,eta){
+  res <- sac.inla(TURNOUT01 ~ 1 + log(GDPCAP), 
+                  d = as.data.frame(turnout), W.rho = W, W.lambda = W,
+                  fhyper = list(prec = list(param = c(0.01, 0.01))),
+                  rho = eta[1],
+                  lambda = eta[2],
+                  family = "gaussian", impacts = FALSE,
+                  control.fixed = list(prec.intercept = 0.001),
+                  control.family = list(hyper = zero.variance),
+                  control.predictor = list(compute = TRUE),
+                  control.compute = list(dic = TRUE, cpo = TRUE),
+                  control.inla = list(print.joint.hyper = TRUE, #), 
+                                      strategy = "laplace", tolerance = 1e-10, h = 0.001),
+                  # Start close to ML estimate
+                  control.mode = list(theta = log(0.2), restart = TRUE),
+                  improve = FALSE,
+                  verbose = FALSE
+  )
+  logdet <- res$logdet
+  res <- inla.rerun(res)
+  res$mlik <- res$mlik + 0.5 * logdet
+  return(list(mlik = res$mlik[[1]],
+              dists = list(intercept = res$marginals.fixed[[1]],
+                           GDPCAP = res$marginals.fixed[[2]],
+                           tau = res$marginals.hyperpar[[1]]),
+              logdet = logdet))
+}
+
+# init = list(mu = c(0,0),cov = diag(2))
+# dq.rho.lambda <- function(y, x, sigma = init$cov, log =TRUE) {
+#   dmvt(y,sigma = sigma, df=3, delta = x, type = "shifted",log=log)
+# }
+# 
+# rq.rho.lambda <- function(x, sigma = init$cov) {
+#   as.vector(rmvt(1,sigma = sigma, df=3, delta = x, type = "shifted"))
+# }
+# 
 # source("./sem/amis_w_inla.R")
 # amis_w_inla_mod <- amis.w.inla(data = turnout, init = init, prior.rho.lambda,
 #                                dq.rho.lambda, rq.rho.lambda, fit.inla,
@@ -50,49 +86,44 @@ source("./sem/general_functions.R")
 # is_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_is$x, y=eta_kern_is$y), z=as.vector(eta_kern_is$z))
 # save(is_w_inla_mod, file = "./sims/sem-is-w-inla.Rdata")
 
+
+
 init = list(mu = c(0,0),cov = 0.5*diag(2))
+
+dq.rho.lambda <- function(y, x, sigma = init$cov, log =TRUE) {
+  dmvnorm(y, mean = x, sigma = sigma, log = log)
+}
+
+rq.rho.lambda <- function(x, sigma = init$cov) {
+  as.vector(rmvnorm(1, mean = x, sigma = sigma))
+}
+
 source("./sem/mcmc_w_inla.R")
 mcmc_w_inla_mod <- mcmc.w.inla(data = turnout, init = init,
                                prior.rho.lambda, dq.rho.lambda, rq.rho.lambda, fit.inla,
-                               n.samples = 1000, n.burnin = 500, n.thin = 1)
+                               n.samples = 10500, n.burnin = 500, n.thin = 1)
 save(mcmc_w_inla_mod, file = "./sims/sem-mcmc-w-inla.Rdata")
 eta_kern_mcmc = kde2d.weighted(x = mcmc_w_inla_mod$eta[,1], y = mcmc_w_inla_mod$eta[,2], w = rep(1,nrow(mcmc_w_inla_mod$eta))/nrow(mcmc_w_inla_mod$eta), n = 100, lims = c(-1,1,-1,1))
 mcmc_w_inla_mod$eta_kern = data.frame(expand.grid(x=eta_kern_mcmc$x, y=eta_kern_mcmc$y), z=as.vector(eta_kern_mcmc$z))
 save(mcmc_w_inla_mod, file = "./sims/sem-mcmc-w-inla.Rdata")
 
 
-# library(ggplot2)
+# mcmc_mod <- spBreg_sac(TURNOUT01 ~ log(GDPCAP), data = as.data.frame(turnout),
+#                        listw = it.lw, control = list(ndraw = 110000L, nomit = 10000L, thin = 10L,
+#                                                      prior = list(nu = 0.01, d0 = 0.01, a1 = 1, a2 = 1,
+#                                                                   rho = sac1$rho, lambda = sac1$lambda, Tbeta = diag(2) * 1000)))
 # 
-# p1 <- ggplot() +
-#   geom_line(data = amis_w_inla_mod$margs$intercept, aes(x=x,y=y,color="AMIS with INLA")) +
-#   geom_line(data = is_w_inla_mod$margs$intercept, aes(x=x,y=y,color="IS with INLA")) +
-#   theme_bw() +
-#   theme(legend.position="bottom")
-# p1
-# 
-# 
-# p2 <- ggplot() +
-#   geom_line(data = amis_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="AMIS with INLA")) +
-#   geom_line(data = is_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="IS with INLA")) +
-#   theme_bw() +
-#   theme(legend.position="bottom")
-# p2
-# 
-# p3 <- ggplot() +
-#   geom_line(data = amis_w_inla_mod$margs$tau, aes(x=x,y=y,color="AMIS with INLA")) +
-#   geom_line(data = is_w_inla_mod$margs$tau, aes(x=x,y=y,color="IS with INLA")) +
-#   theme_bw() +
-#   theme(legend.position="bottom")
-# p3
-# 
-# p4 <- ggplot() +
-#   geom_contour(data = amis_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA"),bins = 6) +
-#   geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) +
-#   labs(color = "",x=expression(rho),y=expression(lambda),linetype="") +
-#   theme_bw() +
-#   theme(legend.position="bottom")
-# p4
-# 
-# spplot(turnout["TURNOUT01"],  col.regions= hcl.colors(16,palette =  hcl.pals("sequential")[21], alpha = 1, rev = TRUE),colorkey = list(space = "bottom"))
-# 
-# spplot(turnout["GDPCAP"], col.regions= hcl.colors(16,palette =  hcl.pals("sequential")[21], alpha = 1, rev = TRUE),colorkey = list(space = "bottom"))
+# mcmc_mod = list(samples = as.data.frame(mcmc_mod),
+#                 times = attributes(mcmc_mod)$timings)
+# mcmc_mod$samples$tau = 1/mcmc_mod$samples$sige
+# eta_kern_mc = kde2d.weighted(x = mcmc_mod$samples$rho, y = mcmc_mod$samples$lambda, w = rep(1,nrow(mcmc_mod$samples))/nrow(mcmc_mod$samples), n = 100, lims = c(-1,1,-1,1))
+# mcmc_mod$eta_kern = data.frame(expand.grid(x=eta_kern_mc$x, y=eta_kern_mc$y), z=as.vector(eta_kern_mc$z))
+# mcmc_mod$margs = lapply(seq(ncol(mcmc_mod$samples)), function(x){
+#   as.data.frame(density(x = mcmc_mod$samples[,x],
+#                         weights = rep(1/nrow(mcmc_mod$samples),nrow(mcmc_mod$samples)),
+#                         kernel = "gaussian")[c(1,2)])
+# })
+# names(mcmc_mod$margs) = names(mcmc_mod$samples)
+# save(mcmc_mod,file = "./sims/sem/sem-mcmc.Rdata")
+
+
