21c5b0766bfb0cf6e75aae65f772974706148f6e
Martin Outzen Berild <martinob@null08.math.ntnu.no>
2020-06-13 09:09:44 +0200

    add mcmc w inla to figures

--- sem/plot_sem.R
+++ sem/plot_sem.R
@@ -2,11 +2,13 @@ library(ggplot2)
 library(ggpubr)
 load("./sims/sem/sem-amis-w-inla.Rdata")
 load("./sims/sem/sem-is-w-inla.Rdata")
+load("./sims/sem/sem-mcmc-w-inla.Rdata")
 load("./sims/sem/sem-mcmc.Rdata")
 
 p1 <- ggplot() +
   geom_line(data = amis_w_inla_mod$margs$intercept, aes(x=x,y=y,color="AMIS with INLA")) +
   geom_line(data = is_w_inla_mod$margs$intercept, aes(x=x,y=y,color="IS with INLA")) +
+  geom_line(data = mcmc_w_inla_mod$margs$intercept, aes(x=x,y=y,color = "MCMC with INLA") ) + 
   geom_line(data = mcmc_mod$margs$`(Intercept)`, aes(x=x,y=y,color = "MCMC")) + 
   labs(color = "",x=expression(beta[0]),y="") +
   theme_bw() +
@@ -17,6 +19,7 @@ p1
 p2 <- ggplot() +
   geom_line(data = amis_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="AMIS with INLA")) +
   geom_line(data = is_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="IS with INLA")) +
+  geom_line(data = mcmc_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="MCMC with INLA")) +
   geom_line(data = mcmc_mod$margs$`log(GDPCAP)`, aes(x=x,y=y,color = "MCMC")) + 
   labs(color = "",x=expression(beta[1]),y="") +
   theme_bw() +
@@ -26,6 +29,7 @@ p2
 p3 <- ggplot() +
   geom_line(data = amis_w_inla_mod$margs$tau, aes(x=x,y=y,color="AMIS with INLA")) +
   geom_line(data = is_w_inla_mod$margs$tau, aes(x=x,y=y,color="IS with INLA")) +
+  geom_line(data = mcmc_w_inla_mod$margs$tau, aes(x=x,y=y,color="MCMC with INLA")) +
   geom_line(data = mcmc_mod$margs$tau, aes(x=x,y=y,color="MCMC")) +
   labs(color = "",x=expression(tau),y="") +
   theme_bw() +
@@ -35,6 +39,7 @@ p3
 p4 <- ggplot() +
   geom_contour(data = amis_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA"),bins = 6) +
   geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) +
+  geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA"),bins = 6) +
   geom_contour(data = mcmc_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC"),bins = 6) +
   labs(color = "",x=expression(rho),y=expression(lambda),linetype="") +
   theme_bw() +
