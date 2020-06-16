f4f1cf9345c537764df7093ab78d347d6a3332f8
Martin Outzen Berild <martinob@null08.math.ntnu.no>
2020-06-16 18:15:31 +0200

    fix colors in joint plot

--- sem/plot_sem.R
+++ sem/plot_sem.R
@@ -4,13 +4,19 @@ load("./sims/sem/sem-amis-w-inla.Rdata")
 load("./sims/sem/sem-is-w-inla.Rdata")
 load("./sims/sem/sem-mcmc-w-inla.Rdata")
 load("./sims/sem/sem-mcmc.Rdata")
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+col_temp = gg_color_hue(4)
 
 p1 <- ggplot() +
   geom_line(data = amis_w_inla_mod$margs$intercept, aes(x=x,y=y,color="AMIS with INLA")) +
   geom_line(data = is_w_inla_mod$margs$intercept, aes(x=x,y=y,color="IS with INLA")) +
-  geom_line(data = mcmc_w_inla_mod$margs$intercept, aes(x=x,y=y,color = "MCMC with INLA") ) + 
+  #geom_line(data = mcmc_w_inla_mod$margs$intercept, aes(x=x,y=y,color = "MCMC with INLA") ) + 
   geom_line(data = mcmc_mod$margs$`(Intercept)`, aes(x=x,y=y,color = "MCMC")) + 
   labs(color = "",x=expression(beta[0]),y="") +
+  scale_color_manual(values=col_temp) + 
   theme_bw() +
   theme(legend.position="bottom")
 p1
@@ -19,9 +25,10 @@ p1
 p2 <- ggplot() +
   geom_line(data = amis_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="AMIS with INLA")) +
   geom_line(data = is_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="IS with INLA")) +
-  geom_line(data = mcmc_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="MCMC with INLA")) +
+  #geom_line(data = mcmc_w_inla_mod$margs$GDPCAP, aes(x=x,y=y,color="MCMC with INLA")) +
   geom_line(data = mcmc_mod$margs$`log(GDPCAP)`, aes(x=x,y=y,color = "MCMC")) + 
   labs(color = "",x=expression(beta[1]),y="") +
+  scale_color_manual(values=col_temp) +
   theme_bw() +
   theme(legend.position="bottom")
 p2
@@ -29,9 +36,10 @@ p2
 p3 <- ggplot() +
   geom_line(data = amis_w_inla_mod$margs$tau, aes(x=x,y=y,color="AMIS with INLA")) +
   geom_line(data = is_w_inla_mod$margs$tau, aes(x=x,y=y,color="IS with INLA")) +
-  geom_line(data = mcmc_w_inla_mod$margs$tau, aes(x=x,y=y,color="MCMC with INLA")) +
+  #geom_line(data = mcmc_w_inla_mod$margs$tau, aes(x=x,y=y,color="MCMC with INLA")) +
   geom_line(data = mcmc_mod$margs$tau, aes(x=x,y=y,color="MCMC")) +
   labs(color = "",x=expression(tau),y="") +
+  scale_color_manual(values=col_temp) +
   theme_bw() +
   theme(legend.position="bottom")
 p3
@@ -39,9 +47,10 @@ p3
 p4 <- ggplot() +
   geom_contour(data = amis_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA"),bins = 6) +
   geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) +
-  geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA"),bins = 6) +
+  #geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA"),bins = 6) +
   geom_contour(data = mcmc_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC"),bins = 6) +
   labs(color = "",x=expression(rho),y=expression(lambda),linetype="") +
+  scale_color_manual(values=col_temp) +
   theme_bw() +
   theme(legend.position="bottom")
 p4
@@ -56,3 +65,4 @@ sp2
 sptot <- ggarrange(sp1, sp2,ncol=2, nrow=1, common.legend = F)
 sptot
 
+calc.theta(is_w_inla_mod$theta,is_w_inla_mod$weight,is_w_inla_mod$eta,nrow(is_w_inla_mod$eta),2)
