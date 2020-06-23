19474807252c2c430165d8552248e6145c496e3f
Berild <moutzebe@gmail.com>
2020-06-24 00:53:13 +0200

    fix figures for frailty problem

--- survival/plot_frailty.R
+++ survival/plot_frailty.R
@@ -18,31 +18,26 @@ res_inla =inla(formula,
 
 
 p1 <- ggplot() + 
-  geom_line(data = amis_w_inla_mod$margs$intercept, aes(x=x,y=y,color = "AMIS w/ INLA")) + 
+  geom_line(data = amis_w_inla_mod$margs$intercept, aes(x=x,y=y),color = gg_color_hue(1)) + 
   #geom_line(data = as.data.frame(res_inla$marginals.fixed[[1]]),aes(x=x,y=y,color = "INLA")) + 
   geom_vline(xintercept = amis_w_inla_mod$params$intercept,color = "firebrick")+ 
-  labs(title = "Intercept") +
-  labs(color = "",x="",y="")+
-  coord_cartesian(xlim=c(-1,5)) + 
+  labs(x = expression(beta[0]),y="") +
   theme_bw()
 p1
 
 p2 <- ggplot() + 
-  geom_line(data = amis_w_inla_mod$margs$beta, aes(x=x,y=y,color = "AMIS w/ INLA")) +
+  geom_line(data = amis_w_inla_mod$margs$beta, aes(x=x,y=y),color = gg_color_hue(1)) +
   #geom_line(data = as.data.frame(res_inla$marginals.fixed[[2]]),aes(x=x,y=y,color = "INLA")) + 
   geom_vline(xintercept = amis_w_inla_mod$params$beta,color = "firebrick")+
-  labs(title= "Beta") + 
-  labs(color = "",x="",y="")+
-  coord_cartesian(xlim=c(1.5,3.3)) + 
+  labs(x= expression(beta[1]),y="") + 
   theme_bw()
 p2
 
 p3 <- ggplot() + 
-  geom_line(data = amis_w_inla_mod$margs$alpha, aes(x=x,y=y,color = "AMIS w/ INLA")) +
+  geom_line(data = amis_w_inla_mod$margs$alpha, aes(x=x,y=y),color = gg_color_hue(1)) +
   #geom_line(data = as.data.frame(res_inla$marginals.hyperpar[[1]]),aes(x=x,y=y,color = "INLA")) + 
   geom_vline(xintercept = amis_w_inla_mod$params$alpha,color = "firebrick")+
-  labs(title= "Alpha") + 
-  labs(color = "",x="",y="")+
+  labs(x= expression(alpha),y="") + 
   theme_bw()
 p3
 
@@ -57,16 +52,17 @@ amis_w_inla_mod$frailty_idx = data.frame(amis_w_inla_mod$frailty_idx, rv = log(a
 p5 <- ggplot() + 
   #geom_ribbon(data = inla_frailty_idx,aes(x =idx,ymin = q0.025, ymax=q0.975),fill = gg_color_hue(3)[2],color = gg_color_hue(3)[2] ,alpha = 0.3,show.legend = FALSE)  + 
   geom_ribbon(data = amis_w_inla_mod$frailty_idx,aes(x =idx, ymin=q0.025, ymax=q0.975),fill = gg_color_hue(3)[1],color = gg_color_hue(3)[1], alpha = 0.3,show.legend = FALSE) +
-  geom_line(data = amis_w_inla_mod$frailty_idx,aes(x =idx,y=q0.5,color = "AMIS w/INLA")) +
+  geom_line(data = amis_w_inla_mod$frailty_idx,aes(x =idx,y=q0.5,color = "AMIS with INLA")) +
   #geom_line(data = inla_frailty_idx, aes(x =idx,y=q0.5,color = "INLA")) +
-  geom_point(data = amis_w_inla_mod$frailty_idx,aes(x =idx,y=q0.5,color = "AMIS w/INLA"),size=2) +
+  geom_point(data = amis_w_inla_mod$frailty_idx,aes(x =idx,y=q0.5,color = "AMIS with INLA"),size=2) +
   #geom_point(data = inla_frailty_idx, aes(x =idx,y=q0.5,color = "INLA")) +
   geom_point(data = amis_w_inla_mod$frailty_idx,aes(x =idx,y = rv,color = "Real Value"),size=2) + 
   labs(y = "",color = "") +
   scale_color_manual(values = c(gg_color_hue(2)[1],"firebrick")) + 
   scale_x_discrete(name ="idx", 
                    limits=amis_w_inla_mod$frailty_idx$idx)+
-  theme_bw()
+  theme_bw() +
+  theme(legend.position="bottom")
 p5
 amis_w_inla_mod$frailty.prec.kde= as.data.frame(density(x = amis_w_inla_mod$frailty.prec,
                                                         weights = amis_w_inla_mod$weight/sum(amis_w_inla_mod$weight),
@@ -74,13 +70,12 @@ amis_w_inla_mod$frailty.prec.kde= as.data.frame(density(x = amis_w_inla_mod$frai
                                                         to = 10,
                                                         kernel = "gaussian")[c(1,2)])
 p6 <- ggplot() + 
-  geom_line(data = amis_w_inla_mod$frailty.prec.kde,aes(x=x,y=y,color = "AMIS w/ INLA")) + 
+  geom_line(data = amis_w_inla_mod$frailty.prec.kde,aes(x=x,y=y),color = gg_color_hue(1)) + 
   #geom_line(data = as.data.frame(res_inla$marginals.hyperpar$`Precision for idx`[-c(74,75),]),aes(x=x,y=y,color = "INLA")) + 
   geom_vline(xintercept = amis_w_inla_mod$params$frailty,color = "firebrick") + 
-  labs(title= "Rate/Shape of frailty") + 
-  labs(color = "")+
+  labs(y="",x=expression(gamma))+
   coord_cartesian(xlim = c(0,10)) +
   theme_bw()
 p6
 
-ggarrange(p1,p2,p3,p5,p6,nrow=3,ncol=2,common.legend = TRUE, legend = "bottom")
+ggarrange(p1,p2,p3,p6,nrow=2,ncol=2)
